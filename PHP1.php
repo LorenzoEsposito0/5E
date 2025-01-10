4583241c82c8a1a051425a205ae6d20610426aca
Lorenzo <121286814+LorenzoEsposito0@users.noreply.github.com>
2025-01-10 13:13:13 +0100

    PHP1

---
+++ PHP1.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>REGG</title>
+</head>
+<body>
+    <label for="name">Cosa si intende per sistema operativo e quali sono le sue principali funzioni in un computer:  </label>
+    <input type="text" id="name" value="your name">
+    <br>
+    <br>
+    <label for="name">Spiega la differenza tra memoria RAM e memoria ROM, fornendo esempi pratici di utilizzo di ciascuna:  </label>
+    <input type="text" id="name" value="your name">
+    <br>
+    <br>
+    <label for="name">Descrivi il processo che consente a un programma scritto in un linguaggio di alto livello (come Python) di essere eseguito su un computer: </label>
+    <input type="text" id="name" value="your name">
+</body>
+</html>
