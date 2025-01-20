74501523bcc36f30dcb4ab29a085ef293cd7dd39
Lorenzo <121286814+LorenzoEsposito0@users.noreply.github.com>
2025-01-21 00:31:39 +0100

    Piccole aggiunte

---
+++ Progetto/Script/scriptCreateLibro.js
@@ -0,0 +1,4 @@
+document.getElementById("FinishCreate").addEventListener('onclick', function (){
+const Titolo = document.getElementById('nomeLibro');
+const Autore = document.getElementById('')
+});
