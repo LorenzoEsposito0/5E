e6beddcd3e985e2436c999931a1ee0453c17188d
Lorenzo <121286814+LorenzoEsposito0@users.noreply.github.com>
2025-01-25 00:39:32 +0100

    Mancano delete e update

---
+++ Progetto/Js/scriptFirstPage.js
@@ -0,0 +1,4 @@
+document.getElementById("BottoneInvio").addEventListener('click', function ()
+{
+   window.location.href='CreaLibro.php';
+});
---
+++ Progetto/Php/UpdateLibro.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../Css/style.css">
+    <title>ciao</title>
+</head>
+<body>
+<nav class="navbar">
+    <h1 class="navbar-title">Menù Libreria</h1>
+    <div class="nav-links">
+        <a href="CreaLibro.php">Crea libro</a>
+        <a href="FirstPage.php">HomePage</a>
+        <a href="LeggiLibri.php">Leggi db</a>
+        <a href="DeleteLibro.php">Cancella Libro</a>
+
+    </div>
+</nav>
+<div class="text">
+    <br>
+</div>
+</body>
+</html>
