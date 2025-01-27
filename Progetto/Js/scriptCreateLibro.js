8b9dfafe17547dd51778cbc23133eec5a2ca7000
Lorenzo <121286814+LorenzoEsposito0@users.noreply.github.com>
2025-01-27 13:18:21 +0100

    Delete Progetto directory

--- Progetto/Js/scriptCreateLibro.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("InviaCreateLibro").addEventListener('click', function ()
-{
-    window.location.href='FirstPage.php';
-});
-
-document.getElementById("BackToFirst").addEventListener('click', function ()
-{
-    window.location.href='FirstPage.php';
-});
--- Progetto/Js/scriptFirstPage.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("BottoneInvio").addEventListener('click', function ()
-{
-   window.location.href='CreaLibro.php';
-});
